eb17d893a98287543b62490b82bea5fcf8c815f1
ShuKadono <kadonoamino@yahoo.co.jp>
2021-03-08 20:11:34 +0900

    new commit

---
+++ inbox/deploy.js
@@ -0,0 +1,22 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+    'motion quality tell chronic inch addict monkey clean moral maid unable broken',
+    'https://rinkeby.infura.io/orDImgKRzwNrVCDrAk5Q' // infura api に登録した際に送られるメールに記載あり,
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .send({ gas:'1000000', from: accounts[0] });
+
+    console.log('Contract deployed to', result.options.address);
+};
+deploy();
